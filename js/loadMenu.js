7f26b95f6fa6a5c1c9e006568ea06adae3fa6552
vnieutin-dev <vnieutin.dev@gmail.com>
2026-03-12 16:45:29 +0100

    styles improvements

--- js/loadMenu.js
+++ js/loadMenu.js
@@ -1,10 +1,7 @@
-// Function to load the menu dynamically from an external HTML file
 function loadMenu() {
-    // Use the fetch API to load the menu.html file
-    fetch('includes/menu.html')  // Make sure the path is correct
-        .then(response => response.text()) // Convert response to text
+    fetch('includes/menu.html')
+        .then(response => response.text())
         .then(data => {
-            // Insert the loaded HTML content into the menu-container div
             document.getElementById('menu-wrapper').innerHTML = data;
         })
         .catch(error => {
