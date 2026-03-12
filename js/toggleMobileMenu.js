cd423b866b7566e26743ec5048845d71696da30b
vnieutin-dev <vnieutin.dev@gmail.com>
2026-03-12 16:05:35 +0100

    improve mobile menu

--- js/toggleMobileMenu.js
+++ js/toggleMobileMenu.js
@@ -1,9 +1,12 @@
 function toggleMobileMenu() {
-    const menu = document.getElementById("mobile-navbar")
+    const menu = document.getElementById("mobile-navbar");
+    const toggle = document.querySelector(".mobile-navbar-toggle");
 
     if (menu.style.display === "flex") {
-        menu.style.display = "none";  // Hide the menu
+        menu.style.display = "none";
+        toggle.textContent = "☰";
     } else {
-        menu.style.display = "flex";  // Show the menu
+        menu.style.display = "flex";
+        toggle.textContent = "✕";
     }
 }
